f01d1d8bfe04cf4f4ce0aa1ff3f8c9515c2e0ca3
Eric Bouck <eric.bouck@gmail.com>
2022-07-16 15:22:11 -0700

    Update hello world to see if deploy works

--- src/tasks/helloWorld.js
+++ src/tasks/helloWorld.js
@@ -2,6 +2,6 @@ defineTask({
   name: "Hello World!",
   description: 'Say hello to the console',
   run: async () => {
-    console.log("Hello World!");
+    console.log("Hello World has been updated!");
   },
 });
