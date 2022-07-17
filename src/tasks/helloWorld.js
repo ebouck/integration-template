7af47cb882e31c543dcba09d00562fe8e46fbe05
Eric Bouck <eric.bouck@gmail.com>
2022-07-17 14:47:02 -0700

    Update the description

--- src/tasks/helloWorld.js
+++ src/tasks/helloWorld.js
@@ -2,7 +2,7 @@ import { defineTask } from "@bigidea/integration-connectors";
 
 defineTask({
   name: "Hello World!",
-  description: "Deployed: Say hello to the console",
+  description: "Say hello to the console",
   run: async () => {
     console.log("Hello World has been updated!");
   },
